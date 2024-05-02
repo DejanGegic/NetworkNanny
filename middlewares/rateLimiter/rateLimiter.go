ad36d90943be219d66f5b60561f21b0b90350719
Dejan Gegic <dejangegic100@gmail.com>
2024-05-02 19:38:23 +0200

    minor fixes

--- middlewares/rateLimiter/rateLimiter.go
+++ middlewares/rateLimiter/rateLimiter.go
@@ -2,6 +2,7 @@ package rateLimiter
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"strconv"
 	"time"
@@ -63,48 +64,51 @@ func New(config LimiterConf) fiber.Handler {
 
 	return func(c *fiber.Ctx) error {
 		ip := c.IP()
-		block := checkIp(ip, config)
+		timer := time.Now()
+		block, ttl := checkIp(ip, config)
+		fmt.Println(time.Now().Sub(timer))
 
 		if block == "perma" {
 			return c.Status(429).SendString("PermaBanned")
 		}
 		if block != "" {
-			return c.Status(429).SendString("Cool down for " + block + ". Refreshing will reset the cooldown. \nRefreshes past this point will result in a ban.")
+			return c.Status(429).SendString("Cool down for " + ttl.Truncate(time.Second).String() + ", until: " + time.Now().Add(ttl).Format(time.Kitchen) + ".\nRefreshes past this point will result in a ban.")
 		}
 		return c.Next()
 	}
 }
 
-func checkIp(ip string, config LimiterConf) (block string) {
+func checkIp(ip string, config LimiterConf) (block string, ttl time.Duration) {
 	// check for PermaBan, return if true
-	res, err := DB.Read("PermaBan" + "|" + ip)
+	res, ttl, err := DB.ReadTTL("PermaBan" + ":" + ip)
 	if err == nil && res != "" {
 		block = "perma"
-		return block
+		l.Info("Requested after PermaBan: " + ip + " by: " + config.LimiterName)
+		return block, ttl
 	}
 
 	// check number of times ip has been requested in last window
-	res, err = DB.Read(config.LimiterName + "|" + ip)
-	if err != nil {
-		// "Key not found" is expected.
-		// Any other error is logged accordingly
-		if err.Error() == "Key not found" {
-			DB.WriteTTL(config.LimiterName+"|"+ip, "1", config.Window)
-		} else {
-			l.Error(err)
-		}
+	res, ttl, err = DB.ReadTTL(config.LimiterName + ":" + ip)
+
+	// if not in db, create it
+	if res == "" {
+		DB.WriteTTL(config.LimiterName+":"+ip, "1", config.Window)
+		return "", config.Window
+	} else if err != nil {
+		l.Error(err)
+		return "", config.Window
 	}
 
 	resInt, _ := strconv.Atoi(res)
 	if resInt >= config.RequestLimit {
 		block = config.Window.String()
 		if resInt >= config.PermaBanThreshold {
-			DB.WriteTTL("PermaBan"+"|"+ip, "1", config.PermaBanTime)
+			DB.WriteTTL("PermaBan"+":"+ip, "1", config.PermaBanTime)
 			l.Warning("PermaBanned: " + ip + " by: " + config.LimiterName + " For: " + config.PermaBanTime.String())
 		}
 	}
 	resInt++
-	DB.WriteTTL(config.LimiterName+"|"+ip, strconv.Itoa(resInt), config.Window)
+	DB.WriteTTL(config.LimiterName+":"+ip, strconv.Itoa(resInt), ttl)
 
-	return block
+	return block, ttl
 }
